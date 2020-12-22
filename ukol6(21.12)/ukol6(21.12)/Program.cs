1e5b8bace96836a87b38674648aa12372cf2276b
Filip Vojtek <f.vojtek01@gmail.com>
2020-12-22 15:30:09 +0100

    ukol6 (2.uloha)

--- ukol6(21.12)/ukol6(21.12)/Program.cs
+++ ukol6(21.12)/ukol6(21.12)/Program.cs
@@ -6,6 +6,7 @@ namespace ukol6_21._12_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Vypis (50) hodnot ");
             /**
              * https://www.itnetwork.cz/csharp/zaklady/c-sharp-tutorial-cykly-for-while
              * vytvoreni promenne o velikosti 50 hodnot
@@ -23,14 +24,34 @@ namespace ukol6_21._12_
                 Polecisel[i] = rnd.Next(-999, 999);
             }
 
-            
+
             for (int i = 0; i < 50; i++)
             {
                 Console.WriteLine(Polecisel[i]);
             }
-               
+            Console.WriteLine("----------------------------------------------");
+            vypis_cisla(Polecisel);          
+            Console.ReadKey();
+
+        }
+        static void vypis_cisla(int[] Polecisel)
+        {
+            Console.Write("Zadej cislo: ");
+            int uzivatelsky_vstup =int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 50; i++)
+            {
+
+                if (Polecisel[i] > uzivatelsky_vstup)
+                {
+                    Console.WriteLine(Polecisel[i]);
+                }
+            }
+
+
             Console.ReadKey();
         }
+
     }
 
 }
