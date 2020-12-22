432adf9ee341e06fd92891b7dad2588f782c6449
Filip Vojtek <f.vojtek01@gmail.com>
2020-12-22 16:24:11 +0100

    ukol6(4uloha)

--- ukol6(21.12)/ukol6(21.12)/Program.cs
+++ ukol6(21.12)/ukol6(21.12)/Program.cs
@@ -6,7 +6,7 @@ namespace ukol6_21._12_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vypis (50) hodnot ");
+            Console.WriteLine("1)Vypis (50) hodnot ");
             /**
              * https://www.itnetwork.cz/csharp/zaklady/c-sharp-tutorial-cykly-for-while
              * vytvoreni promenne o velikosti 50 hodnot
@@ -19,27 +19,32 @@ namespace ukol6_21._12_
             Random rnd = new Random();
 
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < Polecisel.Length; i++)
             {
                 Polecisel[i] = rnd.Next(-999, 999);
             }
 
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < Polecisel.Length; i++)
             {
                 Console.WriteLine(Polecisel[i]);
             }
             Console.WriteLine("----------------------------------------------");
-            vypis_cisla(Polecisel);          
+            vypis_cisla(Polecisel);
+            float prumerna_hodnota_a = prumerna_hodnota(Polecisel);
+             vypis_cisla_vetsi_nez_prumerna_hodnota(Polecisel, prumerna_hodnota_a);
             Console.ReadKey();
 
         }
         static void vypis_cisla(int[] Polecisel)
+
         {
+
+            Console.WriteLine("2) VYPIS VETSIHO CISLA NEZ ZADA UZIVATEL");
             Console.Write("Zadej cislo: ");
             int uzivatelsky_vstup =int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < Polecisel.Length; i++)
             {
 
                 if (Polecisel[i] > uzivatelsky_vstup)
@@ -47,9 +52,38 @@ namespace ukol6_21._12_
                     Console.WriteLine(Polecisel[i]);
                 }
             }
-
-
+            Console.WriteLine("-------------------------------------------");
             Console.ReadKey();
+        }
+        static float prumerna_hodnota(int[] Polecisel)
+        {
+            Console.WriteLine("3) prumerna hodnota");
+            float soucet_cisel = 0;
+
+            for (int i = 0; i < Polecisel.Length; i++)
+            {
+                soucet_cisel += Polecisel[i];
+
+            }
+
+            float prumerna_hodnota = soucet_cisel / Polecisel.Length;
+            Console.WriteLine("prumer: {0}", prumerna_hodnota);
+            Console.WriteLine("---------------------------------------------");
+            Console.ReadKey();
+            return prumerna_hodnota;
+
+        }
+        static void vypis_cisla_vetsi_nez_prumerna_hodnota(int[] Polecisel, float prumerna_hodnota)
+        {
+            int ;
+            for (int i = 0; i < Polecisel.Length; i++);
+            {
+                if (Polecisel)
+                {
+
+                }
+            }
+
         }
 
     }
